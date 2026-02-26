aff35ab4c4f3c3c28b4648fa5f199d080a99f344
Alexandre Matos <matosalexandre795@gmail.com>
2026-02-26 19:15:55 -0300

    ProfileDTO, ProfileService: registerProfile()

---
+++ src/main/java/com/devproject/moneyManager/services/ProfileService.java
@@ -0,0 +1,49 @@
+package com.devproject.moneyManager.services;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.devproject.moneyManager.dto.ProfileDTO;
+import com.devproject.moneyManager.entities.ProfileEntity;
+import com.devproject.moneyManager.repositories.ProfileRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileDTO registerProfile(ProfileDTO profileDTO) {
+        ProfileEntity newProfile = toEntity(profileDTO);
+        newProfile.setActivationToken(UUID.randomUUID().toString());
+        newProfile = profileRepository.save(newProfile);
+        return toDTO(newProfile);
+    }
+
+    public ProfileEntity toEntity(ProfileDTO profileDTO) {
+        return ProfileEntity.builder()
+                .id(profileDTO.getId())
+                .fullName(profileDTO.getFullName())
+                .email(profileDTO.getEmail())
+                .password(profileDTO.getPassword())
+                .profileImageUrl(profileDTO.getProfileImageUrl())
+                .createdAt(profileDTO.getCreatedAt())
+                .updatedAt(profileDTO.getUpdatedAt())
+                .build();
+    }
+
+    public ProfileDTO toDTO(ProfileEntity profileEntity) {
+        return ProfileDTO.builder()
+                .id(profileEntity.getId())
+                .fullName(profileEntity.getFullName())
+                .email(profileEntity.getEmail())
+                .password(profileEntity.getPassword())
+                .profileImageUrl(profileEntity.getProfileImageUrl())
+                .createdAt(profileEntity.getCreatedAt())
+                .updatedAt(profileEntity.getUpdatedAt())
+                .build();
+    }
+}
